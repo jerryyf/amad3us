6e14bde203973ba012def630a174644756843282
jerryyf <jerryyf@linsomnia.net>
2025-04-05 17:50:52 +1100

    add docstrings

--- ingestion/utils.py
+++ ingestion/utils.py
@@ -4,6 +4,16 @@ from datetime import datetime
 DEBUG = True
 
 def format_message_obj(from_id:str, timestamp:str, text_value:str) -> dict:
+    """creates a dict from message parameters
+
+    Args:
+        from_id (str): user id
+        timestamp (str): unix timestamp
+        text_value (str): message content
+
+    Returns:
+        dict: dict containing parameters
+    """
     try:
         dt = datetime.fromtimestamp(float(timestamp))
         return {
@@ -17,31 +27,17 @@ def format_message_obj(from_id:str, timestamp:str, text_value:str) -> dict:
             print(e)
         exit(1)
 
-
-def extract_by_id(from_id:str, filepath:str) -> list:
-    extracted_texts = []
-    with open(filepath, 'r') as file:
-
-        text_data = json.load(file)
-        messages = (text_data.get("messages", None))
-
-        for msg in messages:
-            # check if obj is a message
-            if msg.get("type", None) != "message":
-                continue
-            # else we want to extract the text and timestamp
-            text_value = msg.get("text", None)
-            timestamp = text_data.get("date_unixtime", None)
-            if text_value != None:
-                message_obj = format_message_obj(from_id, timestamp, text_value)
-                extracted_texts.append(message_obj)
-
-    return extracted_texts
-
-
 def extract_all_messages(filepath:str) -> list:
+    """main component for ingesting chat log exports
+
+    Args:
+        filepath (str): path of file
+
+    Returns:
+        list: list of message objects
+    """
     extracted_texts = []
-    with open(filepath, 'r') as file:
+    with open(filepath, "r") as file:
 
         jsonfile = json.load(file)
         messages = (jsonfile.get("messages", None))
@@ -61,6 +57,13 @@ def extract_all_messages(filepath:str) -> list:
     return extracted_texts
 
 def format_output(filepath:str) -> str:
+    """returns a formatted string of all messages in the file
+    Args:
+        filepath (str): path of the file to be formatted
+
+    Returns:
+        str: formatted string
+    """
     ret = ""
     all_msg = extract_all_messages(filepath)
     for i in range(len(all_msg)):
