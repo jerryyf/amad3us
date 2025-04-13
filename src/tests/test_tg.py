20065f54f9edb52a79c2eec09a579d5d3d920dfa
jerryyf <jerryyf@linsomnia.net>
2025-04-13 20:58:22 +1000

    refactor combining of messages

--- src/tests/test_tg.py
+++ src/tests/test_tg.py
@@ -1,9 +1,17 @@
-from ingestion.tg import format_jsonl, extract_all_messages, combine_messages
+from ingestion.tg import format_jsonl, extract_all_messages, combine_messages, handle_user_order
 from os import getenv
+from dotenv import load_dotenv
 
-message_list = extract_all_messages("ingestion/sample.json")
+# init tests
+load_dotenv("../.env")
+tg_username = getenv("TG_USERNAME")
 
-# print(format_jsonl("ingestion/sample.json", "username"))
+# test extracted messages output
 
-cat_msg = combine_messages(message_list, "your-username")
+# test handle user order output
+
+# test combine messages output
+message_list = handle_user_order(extract_all_messages("ingestion/sample.json"))
+
+cat_msg = combine_messages(message_list, tg_username)
 print(cat_msg)
