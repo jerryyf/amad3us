2b2121d0b9b3ec58fcedbc9e611c347c04ad0913
dnzhou <zhou.danny@hotmail.com>
2025-04-20 17:06:46 +1000

    fixed file containing secret issue

--- src/tests/test_tg.py
+++ src/tests/test_tg.py
@@ -1,9 +1,12 @@
-from src.ingestion.tg import format_jsonl, extract_all_messages, combine_messages
+from src.ingestion.tg import format_jsonl, extract_all_messages, combine_messages, handle_user_order
 from os import getenv
 from dotenv import load_dotenv
+#from src.tests.tg_test_utils import 
+
+import unittest
 
 # init tests
-load_dotenv("../.env")
+load_dotenv(".env")
 tg_username = getenv("TG_USERNAME")
 
 # test extracted messages output
@@ -46,7 +49,10 @@ class TestExtractedMessages(unittest.TestCase):
 # test handle user order output
 
 # test combine messages output
-message_list = handle_user_order(extract_all_messages("ingestion/sample.json"))
+#message_list = handle_user_order(extract_all_messages("src/tests/test_logs/basic_receive_message.json"))
 
-cat_msg = combine_messages(message_list, tg_username)
-print(cat_msg)
+#cat_msg = combine_messages(message_list, tg_username)
+#print(cat_msg)
+
+if __name__ == "__main__":
+    unittest.main()
