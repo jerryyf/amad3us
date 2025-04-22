6bd27640f806ac26c22dda4486166332a5147ac9
jerryyf <jerryyf@linsomnia.net>
2025-04-22 18:54:18 +1000

    use relative paths in tests

--- tests/test_tg.py
+++ tests/test_tg.py
@@ -1,23 +1,26 @@
 from ingestion.tg import format_jsonl, extract_all_messages, combine_messages, handle_user_order
-from os import getenv
+import os
 from dotenv import load_dotenv
-
 import unittest
 
-# init tests
+# init env
 load_dotenv("../env")
-tg_username = getenv("TG_USERNAME")
+tg_username = os.getenv("TG_USERNAME")
+
+# init paths
+testdir = os.path.dirname(__file__)
+sampledir = os.path.join(testdir, 'test_logs_tg')
 
 # test extracted messages output
 class TestExtractedMessages(unittest.TestCase):
     def test_message_extraction_contains_single(self):
-        message_list = extract_all_messages("src/tests/test_logs_tg/basic_receive_message.json")
+        message_list = extract_all_messages(f"{sampledir}/basic_receive_message.json")
         # check if the list is not empty
         self.assertTrue(len(message_list) > 0)
         # check if the first message is of type dict
         self.assertIsInstance(message_list[0], dict)
     def test_basic_interaction(self):
-        message_list = extract_all_messages("src/tests/test_logs_tg/basic_response.json")
+        message_list = extract_all_messages(f"{sampledir}/basic_response.json")
         self.assertTrue(len(message_list) == 2)
 
         self.assertTrue(message_list[0]['user'] == "User1")
@@ -29,7 +32,7 @@ class TestExtractedMessages(unittest.TestCase):
         self.assertTrue(message_list[1]['time'] == "08-07-2023, 00:16")
 
     def test_output_one_hundred_messages(self):
-        message_list = extract_all_messages("src/tests/test_logs_tg/one_hundred_messages.json")
+        message_list = extract_all_messages(f"{sampledir}/one_hundred_messages.json")
         self.assertTrue(len(message_list) == 100)
 
         self.assertTrue(message_list[0]['user'] == "User1")
@@ -37,7 +40,7 @@ class TestExtractedMessages(unittest.TestCase):
         self.assertTrue(message_list[99]['user'] == "User2")
 
     def test_sample_conversation(self):
-        message_list = extract_all_messages("src/tests/test_logs_tg/sample_conversation.json")
+        message_list = extract_all_messages(f"{sampledir}/sample_conversation.json")
         print(message_list)
         # Should skip photo message
         self.assertTrue(message_list[0] == "220c per L but paid 179c per L")
@@ -45,13 +48,11 @@ class TestExtractedMessages(unittest.TestCase):
         self.assertTrue(message_list[0]['user'] == "UserA")
         self.assertTrue(message_list[1]['user'] == "UserA")
         self.assertTrue(message_list[2]['user'] == "UserB")
+
 # test handle user order output
 
 # test combine messages output
-#message_list = handle_user_order(extract_all_messages("src/tests/test_logs/basic_receive_message.json"))
 
-#cat_msg = combine_messages(message_list, tg_username)
-#print(cat_msg)
 
 if __name__ == "__main__":
     unittest.main()
